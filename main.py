f98d939869fa57473417bc4d86a17619c3df6274
vishwaraj <vishwarajdp95@gmail.com>
2025-03-25 16:16:32 +0530

    main is empty

---
+++ main.py
@@ -0,0 +1,16 @@
+
+# d = "kslfnskfnskofnsofn"
+#
+# s = {}
+#
+# for i in d:
+#     if i in s:
+#         s[i] +=1
+#     else:
+#         s[i] = 1
+# print(s)
+
+d = ["ab" , "dc"]
+
+print(list(map(list,d)))
+
